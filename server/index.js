07f62be76dc5d05fd7ab6b4e7135087dc4f086c8
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:26:30 -0700

    Add body parser & router to server index file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const reviewRouter = require('./routers/review.js');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.use('/api/review', reviewRouter);
+
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log('trypadvisorReviews service listening on port', port));
