6bcf0a6772f000eaed43fe259d6ab24366ba6bdf
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:04:40 -0700

    Build functions to update Attractions, Reviews, Users, and Photos in db

--- database/controllers.js
+++ database/controllers.js
@@ -1,29 +1,47 @@
 const db = require('./index.js');
 
-// Add new Attraction
-const addAttraction = (attractionData, callback) => {
-  db.Attraction.create(attractionData, callback);
-};
+// Functions to add records
+  const addAttraction = (attractionData, callback) => {
+    db.Attraction.create(attractionData, callback);
+  };
 
-// Add new Review
-const addReview = (reviewData, callback) => {
-  reviewData.reviewDate = new Date();
-  db.Review.create(reviewData, callback);
-};
+  const addReview = (reviewData, callback) => {
+    reviewData.reviewDate = new Date();
+    db.Review.create(reviewData, callback);
+  };
 
-// Add new User
-const addUser = (userData, callback) => {
-  db.User.create(userData, callback);
-};
+  const addUser = (userData, callback) => {
+    db.User.create(userData, callback);
+  };
 
-// Add new Photo
-const addPhoto = (photoData, callback) => {
-  db.Photo.create(photoData, callback);
-};
+  const addPhoto = (photoData, callback) => {
+    db.Photo.create(photoData, callback);
+  };
+
+// Functions to update records
+  const updateAttraction = (attractionId, parameters, callback) => {
+    db.Attraction.findByIdAndUpdate(attractionId, parameters, callback);
+  };
+
+  const updateReview = (reviewId, parameters, callback) => {
+    db.Review.findByIdAndUpdate(reviewId, parameters, callback);
+  };
+
+  const updateUser = (userId, parameters, callback) => {
+    db.User.findByIdAndUpdate(userId, parameters, callback);
+  };
+
+  const updatePhoto = (photoId, parameters, callback) => {
+    db.Photo.findByIdAndUpdate(photoId, parameters, callback);
+  };
 
 module.exports = {
   addAttraction: addAttraction,
   addReview: addReview,
   addUser: addUser,
   addPhoto: addPhoto,
+  updateAttraction: updateAttraction,
+  updateReview: updateReview,
+  updateUser: updateUser,
+  updatePhoto: updatePhoto
 };
