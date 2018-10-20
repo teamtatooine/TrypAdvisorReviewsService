1892236ea15c805788ee0f7dab76b5138241565e
James Lee <james.k.lee4@gmail.com>
2018-10-20 01:06:52 -0700

    Build functions to add new Attraction, Review, User, and Photo to db

--- database/controllers.js
+++ database/controllers.js
@@ -1,1 +1,29 @@
 const db = require('./index.js');
+
+// Add new Attraction
+const addAttraction = (attractionData, callback) => {
+  db.Attraction.create(attractionData, callback);
+};
+
+// Add new Review
+const addReview = (reviewData, callback) => {
+  reviewData.reviewDate = new Date();
+  db.Review.create(reviewData, callback);
+};
+
+// Add new User
+const addUser = (userData, callback) => {
+  db.User.create(userData, callback);
+};
+
+// Add new Photo
+const addPhoto = (photoData, callback) => {
+  db.Photo.create(photoData, callback);
+};
+
+module.exports = {
+  addAttraction: addAttraction,
+  addReview: addReview,
+  addUser: addUser,
+  addPhoto: addPhoto,
+};
