b742db0c35d73164bbb0352cf598acb465c96978
James Lee <james.k.lee4@gmail.com>
2018-10-20 01:20:57 -0700

    Initialize database seeding file

---
+++ seed/seed.js
@@ -0,0 +1,4 @@
+const db = require('../database/index.js');
+const dbAdmin = require('../database/admin.js');
+const dbControllers = require('../database/controllers.js');
+const mockData = require('./mock-data.js');
