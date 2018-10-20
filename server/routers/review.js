fc03d82eb8d51390d0007b5e5162a5b3e961da64
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:48:35 -0700

    Change GET request to POST and add functionality to set filters for reviews

--- server/routers/review.js
+++ server/routers/review.js
@@ -4,17 +4,29 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const db = require('../../database/controllers.js');
 
-// Get request to get all Reviews
+// Post request to GET all or filtered Reviews
 router.route('/:id')
-  .get((req, res) => {
+  .post((req, res) => {
+    const filters = {};
+    filters.attraction = mongoose.Types.ObjectId(req.params.id);
+
+    // Check for Traveler Rating filters
+    const ratings = [];
+    req.body.excellent === 'true' ? ratings.push(5) : null;
+    req.body.verygood === 'true' ? ratings.push(4) : null;
+    req.body.average === 'true' ? ratings.push(3) : null;
+    req.body.poor === 'true' ? ratings.push(2) : null;
+    req.body.terrible === 'true' ? ratings.push(1) : null;
+    if (ratings.length !== 0) {
+      filters.userRating = { $in: ratings };
+    };
+
     // Get Reviews from db based on filters
-    filters = { attraction: mongoose.Types.ObjectId(req.params.id) };
     db.getReviews(filters, (err, result) => {
       if (err) {
         console.log('Error', err);
       } else {
         console.log('Reviews returned', result);
-        console.log('Review Count:', result.length);
         res.send(result);
       };
     })
