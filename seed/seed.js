7927a5535969ba7a00de2b97042e9b75accc3ad7
James Lee <james.k.lee4@gmail.com>
2018-10-20 01:22:46 -0700

    Build function to seed database

--- seed/seed.js
+++ seed/seed.js
@@ -2,3 +2,33 @@ const db = require('../database/index.js');
 const dbAdmin = require('../database/admin.js');
 const dbControllers = require('../database/controllers.js');
 const mockData = require('./mock-data.js');
+
+// Add mock data to database
+const seedDb = () => {
+  for (attraction of mockData.attractions) {
+    dbControllers.addAttraction(attraction, (err, result) => {
+      (err) ? console.log('Error', err) : console.log('Attraction saved to database', result);
+    });
+  };
+  for (review of mockData.reviews) {
+    dbControllers.addReview(review, (err, result) => {
+      (err) ? console.log('Error', err) : console.log('Review saved to database', result);
+    });
+  };
+  for (user of mockData.users) {
+    dbControllers.addUser(user, (err, result) => {
+      (err) ? console.log('Error', err) : console.log('User saved to database', result);
+    });
+  };
+  for (photo of mockData.photos) {
+    dbControllers.addPhoto(photo, (err, result) => {
+      (err) ? console.log('Error', err) : console.log('Photo saved to database', result);
+    });
+  };
+};
+
+// EXECUTION FUNCTIONS
+seedDb();
+dbAdmin.logAll();
+// dbAdmin.logAllAndPopulate();
+// dbAdmin.deleteAll();
