9b5b573709de659cc005cfa2d654a5a45ab6fcdb
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:34:29 -0700

    Stub out get/post/delete requests

--- server/routers/review.js
+++ server/routers/review.js
@@ -4,4 +4,15 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const db = require('../../database/controllers.js');
 
+// Get request to get all Reviews
+router.route('/:id')
+  .get((req, res) => {});
+
+// Post request to add new Review
+router.route('/:id/add')
+  .post((req, res) => {});
+
+router.route('/:id/delete')
+  .delete((req, res) => {});
+
 module.exports = router;
