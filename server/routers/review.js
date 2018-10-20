749d09e4a543d21bf65998e33a18cc772f7275ba
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:43:58 -0700

    Build GET request for reviews

--- server/routers/review.js
+++ server/routers/review.js
@@ -6,7 +6,19 @@ const db = require('../../database/controllers.js');
 
 // Get request to get all Reviews
 router.route('/:id')
-  .get((req, res) => {});
+  .get((req, res) => {
+    // Get Reviews from db based on filters
+    filters = { attraction: mongoose.Types.ObjectId(req.params.id) };
+    db.getReviews(filters, (err, result) => {
+      if (err) {
+        console.log('Error', err);
+      } else {
+        console.log('Reviews returned', result);
+        console.log('Review Count:', result.length);
+        res.send(result);
+      };
+    })
+  });
 
 // Post request to add new Review
 router.route('/:id/add')
