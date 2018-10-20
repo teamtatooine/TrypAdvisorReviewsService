e3a30ea72d5a3fa73a0eb979ed205edf35d7f058
James Lee <james.k.lee4@gmail.com>
2018-10-20 00:02:51 -0700

    Build Attraction schema

--- database/index.js
+++ database/index.js
@@ -1,9 +1,36 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trypadvisorReviews');
 
-// check db connection
+// Check db connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongoose connection error:'));
 db.once('open', function() {
   console.log('Mongoose connected!');
+});
+
+// Define mongoose schemas
+let Schema = mongoose.Schema;
+
+let attractionSchema = new Schema({
+  name: String,
+  description: String,
+  phone: String,
+  email: String,
+  website: String,
+  suggestedDuration: Number,
+  featuredIn: String,
+  address1: String,
+  address2: String,
+  city: String,
+  state: String,
+  zip: String,
+  lat: Number,
+  lng: Number,
+  category: String,
+  photos: [
+    {type: Schema.Types.ObjectId, ref: 'Photo'}
+  ],
+  reviews: [
+    {type: Schema.Types.ObjectId, ref: 'Review'}
+  ]
 });
