905cc013fb01aa3ff116c4b15660e923c9ea8b03
James Lee <james.k.lee4@gmail.com>
2018-10-20 02:31:00 -0700

    Initialize server router directory & file

---
+++ server/routers/review.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const db = require('../../database/controllers.js');
+
+module.exports = router;
