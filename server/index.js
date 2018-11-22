3a329558529dd2cf6b10759d5b03fb1c87da7eb1
James Lee <james.k.lee4@gmail.com>
2018-11-22 05:16:19 -0800

    Add CORS support

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const path = require('path');
 const reviewRouter = require('./routers/review.js');
-require('dotenv').config();
 
 const app = express();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/../../../TrypAdvisorClient/dist'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  next();
+});
 
 app.use('/api/review', reviewRouter);
 
-const port = process.env.PORT || 3000
+const port = 4000
 app.listen(port, () => console.log('trypadvisorReviews service listening on port', port));
